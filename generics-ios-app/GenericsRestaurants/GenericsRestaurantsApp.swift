1a875f21f74254b3022396eb7224a81cd29fa682
Mike JR <mikedalvik@ya.ru>
2023-02-14 22:00:55 +0100

    Added target for restaurant app and change platforms

---
+++ generics-ios-app/GenericsRestaurants/GenericsRestaurantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericsRestaurantsApp.swift
+//  GenericsRestaurants
+//
+//  Created by Mike Shevelinsky on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct GenericsRestaurantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
