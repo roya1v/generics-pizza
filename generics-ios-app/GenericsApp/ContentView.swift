ffd6b1431cfd649d102d816a86db445f137dd696
Mike <m.shevelinski@gmail.com>
2023-03-22 22:24:15 +0100

    Update cart view tab

--- generics-ios-app/GenericsApp/ContentView.swift
+++ generics-ios-app/GenericsApp/ContentView.swift
@@ -19,6 +19,10 @@ struct ContentView: View {
                 .tabItem {
                     Label("Menu", systemImage: "menucard")
                 }
+            CartView()
+                .tabItem {
+                    Label("Cart", systemImage: "cart")
+                }
         }
     }
 }
