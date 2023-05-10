ccb3f4a012f9f360a95e728895a3632ab039b634
Mike <m.shevelinski@gmail.com>
2023-05-10 22:23:00 +0200

    Some more progress on that address view (NOT FINISHED)

--- generics-ios-app/GenericsApp/Cart/Address/AddressSheetView.swift
+++ generics-ios-app/GenericsApp/Cart/Address/AddressSheetView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 final class AddressSheetView: UIView {
 
@@ -21,7 +22,7 @@ final class AddressSheetView: UIView {
         return view
     }()
 
-    private let addressTextField: UITextField = {
+    let addressTextField: UITextField = {
         let view = UITextField(frame: .zero)
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
@@ -33,11 +34,13 @@ final class AddressSheetView: UIView {
         return view
     }()
 
-    private let routeFinishStep: RouteStepView = {
+    let routeFinishStep: RouteStepView = {
         let view = RouteStepView()
         view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
+
+    var texting = PassthroughSubject<String?, Never>()
 
     init() {
         super.init(frame: .zero)
@@ -71,15 +74,17 @@ final class AddressSheetView: UIView {
             addressTextField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
             addressTextField.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 16.0),
         ])
-        addressTextField.backgroundColor = .red
+        addressTextField.borderStyle = .roundedRect
 
         addSubview(routeStartStep)
         NSLayoutConstraint.activate([
             routeStartStep.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16.0),
             routeStartStep.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16.0),
-            routeStartStep.topAnchor.constraint(equalTo: addressTextField.bottomAnchor, constant: 16.0),
-            //routeStartStep.bottomAnchor.constraint(equalTo: submitButton.topAnchor, constant: -16.0)
+            routeStartStep.topAnchor.constraint(equalTo: addressTextField.bottomAnchor, constant: 16.0)
         ])
+        routeStartStep.titleText = "Restaurant"
+        routeStartStep.subtitleText = "Generic's street 8"
+        routeStartStep.image = UIImage(named: "destination_icon")
 
         addSubview(routeFinishStep)
         NSLayoutConstraint.activate([
@@ -88,6 +93,13 @@ final class AddressSheetView: UIView {
             routeFinishStep.topAnchor.constraint(equalTo: routeStartStep.bottomAnchor, constant: 16.0),
             routeFinishStep.bottomAnchor.constraint(equalTo: submitButton.topAnchor, constant: -16.0)
         ])
+        routeFinishStep.titleText = "You"
+        routeFinishStep.subtitleText = "Generic's street 9"
+        routeFinishStep.image = UIImage(named: "location_icon")
+
+        addressTextField.addAction(.init(handler: { action in
+            self.texting.send(self.addressTextField.text)
+        }), for: .editingChanged)
     }
 
     required init?(coder: NSCoder) {
@@ -116,6 +128,33 @@ final class RouteStepView: UIView {
         return view
     }()
 
+    var titleText: String? {
+        get {
+            titleView.text
+        }
+        set {
+            titleView.text = newValue
+        }
+    }
+
+    var subtitleText: String? {
+        get {
+            subtitleView.text
+        }
+        set {
+            subtitleView.text = newValue
+        }
+    }
+
+    var image: UIImage? {
+        get {
+            imageView.image
+        }
+        set {
+            imageView.image = newValue
+        }
+    }
+
     init() {
         super.init(frame: .zero)
 
@@ -125,18 +164,34 @@ final class RouteStepView: UIView {
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             imageView.topAnchor.constraint(equalTo: topAnchor),
             imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
-            imageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1.0 / 5.0)
+            //imageView.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 1.0 / 5.0)
         ])
         imageView.backgroundColor = .red
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        imageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+        imageView.layer.cornerRadius = 16.0
+        imageView.layer.masksToBounds = true
 
         addSubview(titleView)
         NSLayoutConstraint.activate([
             titleView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8.0),
-            titleView.topAnchor.constraint(equalTo: topAnchor, constant: 8.0),
-            titleView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 8.0),
+            titleView.topAnchor.constraint(equalTo: topAnchor, constant: 4.0),
+            titleView.trailingAnchor.constraint(equalTo: trailingAnchor),
         ])
-        titleView.backgroundColor = .purple
-        titleView.text = "TEST"
+
+        titleView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        //titleView.font = .preferredFont(forTextStyle: .title2)
+
+        addSubview(subtitleView)
+        NSLayoutConstraint.activate([
+            subtitleView.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8.0),
+            subtitleView.topAnchor.constraint(equalTo: titleView.bottomAnchor, constant: 2.0),
+            subtitleView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            subtitleView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -4.0)
+        ])
+
+        subtitleView.setContentHuggingPriority(.defaultHigh, for: .vertical)
+        subtitleView.textColor = .systemGray
 
     }
 
--- generics-ios-app/GenericsApp/Cart/Address/AddressViewController.swift
+++ generics-ios-app/GenericsApp/Cart/Address/AddressViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import MapKit
+import Combine
+import Contacts
 
 final class AddressViewController: UIViewController {
 
@@ -38,12 +40,16 @@ final class AddressViewController: UIViewController {
 
     var constraint: NSLayoutConstraint! = nil
 
+    var cancellable = Set<AnyCancellable>()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupView()
         mapView.delegate = self
     }
+
+    let geocoder = CLGeocoder()
 
     private func setupView() {
         view.addSubview(mapView)
@@ -62,6 +68,42 @@ final class AddressViewController: UIViewController {
         ])
         constraint = addressSheetView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         constraint.isActive = true
+
+        view.addSubview(closeButtonView)
+        NSLayoutConstraint.activate([
+            closeButtonView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 8.0),
+            closeButtonView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8.0),
+        ])
+
+        closeButtonView.backgroundColor = .white
+        closeButtonView.setImage(UIImage(systemName: "chevron.left"), for: .normal)
+        closeButtonView.tintColor = .black
+        closeButtonView.configuration?.contentInsets = .init(top: 18.0, leading: 18.0, bottom: 18.0, trailing: 18.0)
+
+        addressSheetView.texting.debounce(for: .seconds(2), scheduler: DispatchQueue.main).sink { value in
+            self.geocoder.geocodeAddressString(value ?? "") {
+                    (placemarks, error) in
+                    guard error == nil else {
+                        print("Geocoding error: \(error!)")
+                        return
+                    }
+                let place = placemarks!.first!
+                self.mapView.setRegion(.init(center: place.location!.coordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0), animated: true)
+                //placemarks!.first!.
+                let formatter = CNPostalAddressFormatter()
+                let addressString = formatter.string(from: place.postalAddress!)
+                self.addressSheetView.routeFinishStep.subtitleText = addressString
+                }
+        }.store(in: &cancellable)
+
+        view.addSubview(pinView)
+        NSLayoutConstraint.activate([
+            pinView.bottomAnchor.constraint(equalTo: view.centerYAnchor),
+            pinView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            pinView.heightAnchor.constraint(equalToConstant: 80.0),
+            pinView.widthAnchor.constraint(equalTo: pinView.heightAnchor)
+        ])
+        pinView.image = UIImage(systemName: "mappin")
     }
 }
 
@@ -77,6 +119,15 @@ extension AddressViewController: MKMapViewDelegate {
         constraint.constant = 0
         UIView.animate(withDuration: 0.1, delay: 0, options: .curveEaseOut) {
             self.view.layoutIfNeeded()
+        }
+
+
+        geocoder.reverseGeocodeLocation(CLLocation(latitude: mapView.region.center.latitude, longitude: mapView.region.center.longitude)) { places, error in
+            let place = places!.first!
+            let formatter = CNPostalAddressFormatter()
+            let addressString = formatter.string(from: place.postalAddress!)
+            self.addressSheetView.routeFinishStep.subtitleText = addressString
+            self.addressSheetView.addressTextField.text = addressString
         }
     }
 }
