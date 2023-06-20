2d69aef4eee2990f6a50907c8829bb4806379d04
Mike <m.shevelinski@gmail.com>
2023-06-20 19:19:05 +0200

    Add subtotal model

---
+++ generics-ios-app/GenericsPackage/Sources/GenericsModels/SubtotalModel.swift
@@ -0,0 +1,20 @@
+//
+//  SubtotalModel.swift
+//  
+//
+//  Created by Mike Shevelinsky on 20/06/2023.
+//
+
+import Foundation
+
+public struct SubtotalModel: Codable {
+    public init(name: String, value: Int, isSecondary: Bool = true) {
+        self.name = name
+        self.value = value
+        self.isSecondary = isSecondary
+    }
+
+    public let name: String
+    public let value: Int
+    public let isSecondary: Bool
+}
