98c4a2c408cd3bdc5b4735a24a699958fc11235a
Mike <m.shevelinski@gmail.com>
2023-07-22 18:08:10 +0200

    Start working on card system

--- generics-ios-app/GenericsApp/Home/HomeView.swift
+++ generics-ios-app/GenericsApp/Home/HomeView.swift
@@ -10,90 +10,76 @@ import GenericsUI
 
 struct HomeView: View {
 
-    @State var isShowing = false
-    @Namespace var heroAnimation: Namespace.ID
-
     var body: some View {
         NavigationView {
-        if isShowing {
-            ScrollView(.vertical) {
-                ZStack(alignment: .topTrailing) {
-                    Image("pizza")
-                        .resizable()
-                        .scaledToFit()
-                        .transition(.scale)
-                        .matchedGeometryEffect(id: "image", in: heroAnimation)
-                    Button {
-                        withAnimation(.spring()) {
-                            isShowing.toggle()
-                        }
-                    } label: {
-                        Image(systemName: "xmark.circle.fill")
-                            .font(.system(size: 24.0))
-                            .symbolRenderingMode(.palette)
-                            .foregroundStyle(.white, .black.opacity(0.5))
-
-                    }
-                    .offset(x: -30, y: 25)
-                }
-
-                VStack(alignment: .leading) {
-                    Text("OFFER")
-                        .font(.system(size: 16.0, weight: .medium))
-                        .foregroundColor(.gray)
-                        .matchedGeometryEffect(id: "headline", in: heroAnimation)
-                    Text("Enjoy the rustic taste of countryside")
-                        .font(.system(size: 28.0, weight: .bold))
-                        .matchedGeometryEffect(id: "title", in: heroAnimation)
-                    Text("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
-                    )
-                    .font(.system(size: 16.0, weight: .regular))
-                    .foregroundColor(.gray)
-                }
-                .padding()
-                Spacer()
-            }
-            .statusBarHidden(isShowing)
-            .ignoresSafeArea()
-        } else {
-            ScrollView(.vertical) {
+            ScrollView {
                 CardView {
-                    VStack(alignment: .leading) {
+                    VStack {
                         Image("pizza")
                             .resizable()
                             .scaledToFit()
-                            .transition(.scale)
-
-                        VStack(alignment: .leading) {
-                            Text("OFFER")
-                                .font(.system(size: 16.0, weight: .medium))
-                                .foregroundColor(.gray)
-                                .matchedGeometryEffect(id: "headline", in: heroAnimation)
-                            Text("Enjoy the rustic taste of countryside")
-                                .font(.system(size: 28.0, weight: .bold))
-                                .matchedGeometryEffect(id: "title", in: heroAnimation)
-                            Text("The mushroom pizza will give you the enjoyment of fresh air. Order now."
-                            )
-                            .font(.system(size: 16.0, weight: .regular))
-                            .foregroundColor(.gray)
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Lorem ipsum")
+                                    .font(.title)
+                                Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit... ")
+                                    .multilineTextAlignment(.leading)
+                            }
+                            Button("CTA") {
+                                print("CTA")
+                            }
+                            .buttonStyle(.bordered)
                         }
-                        .padding()
+                        .padding([.leading, .trailing, .bottom], 10.0)
                     }
                 }
-                .matchedGeometryEffect(id: "image", in: heroAnimation)
                 .padding()
-                .onTapGesture {
-                    withAnimation(.spring()) {
-                        isShowing.toggle()
+
+                CardView {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Lorem ipsum")
+                                .font(.title)
+                            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit... ")
+                                .font(.caption)
+                                .multilineTextAlignment(.leading)
+                        }
+                        Button("CTA") {
+                            print("CTA")
+                        }
+                        .buttonStyle(.bordered)
                     }
+                    .padding()
                 }
+                .padding()
+
+                CardView {
+                    VStack(alignment: .leading) {
+                        Text("Lorem ipsum")
+                            .font(.title)
+                        HStack {
+                            Image("menu_pizza")
+                                .resizable()
+                                .scaledToFit()
+                            Image("menu_pizza")
+                                .resizable()
+                                .scaledToFit()
+                            Image("menu_pizza")
+                                .resizable()
+                                .scaledToFit()
+                            Image("menu_pizza")
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
+                    .padding([.leading, .trailing, .bottom], 10.0)
+                }
+                .padding()
             }
-            .navigationTitle("Home")
-            .navigationBarTitleDisplayMode(.large)
-
-
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color(.systemGray6))
+            .navigationTitle("Welcome back")
         }
-    }
     }
 }
 
