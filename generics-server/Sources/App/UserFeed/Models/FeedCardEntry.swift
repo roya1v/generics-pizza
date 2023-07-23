c21a148598e941931e4eef3303ebeade0017fc5a
Mike <m.shevelinski@gmail.com>
2023-07-23 15:24:07 +0200

    Start implementing backend

--- generics-server/Sources/App/UserFeed/Models/FeedCardEntry.swift
+++ generics-server/Sources/App/UserFeed/Models/FeedCardEntry.swift
@@ -1,5 +1,5 @@
 //
-//  FeedCard.swift
+//  FeedCardEntry.swift
 //  
 //
 //  Created by Mike S. on 23/07/2023.
@@ -8,13 +8,33 @@
 import Fluent
 import Vapor
 
-final class FeedCard: Model, Content {
+final class FeedCardEntry: Model, Content {
     static var schema = "feed_cards"
 
     @ID(key: .id)
     var id: UUID?
 
+    @Field(key: "title")
+    var title: String?
 
+    @Field(key: "subtitle")
+    var subtitle: String?
+
+    @Field(key: "cta")
+    var cta: String?
+
+    @Field(key: "image")
+    var image: String?
 
     init() { }
+
+    init(title: String? = nil,
+         subtitle: String? = nil,
+         cta: String? = nil,
+         image: String? = nil) {
+        self.title = title
+        self.subtitle = subtitle
+        self.cta = cta
+        self.image = image
+    }
 }
