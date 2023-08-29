48fd7170c3be22d8a25552dabe7262884a536271
Mike <m.shevelinski@gmail.com>
2023-08-29 22:01:18 +0200

    Add custom font for order sticker view

--- generics-ios-app/GenericsDriver/OrderStickerView.swift
+++ generics-ios-app/GenericsDriver/OrderStickerView.swift
@@ -30,6 +30,9 @@ final class OrderStickerView: UIView {
     }
 
     private func setupView() {
+        let font = UIFont(name: "DotMatrix", size: UIFont.labelFontSize)!
+        nameLabel.font = font.withSize(32.0)
+        orderNumberLabel.font = font.withSize(32.0)
         backgroundColor = .init(red: 245.0 / 255.0, green: 245.0 / 255.0, blue: 220.0 / 255.0, alpha: 1.0)
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -37,10 +40,10 @@ final class OrderStickerView: UIView {
 
         addSubview(stackView)
         NSLayoutConstraint.activate([
-            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
-            stackView.topAnchor.constraint(equalTo: topAnchor),
-            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: .big),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -.big),
+            stackView.topAnchor.constraint(equalTo: topAnchor, constant: .big),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -.big)
         ])
 
         stackView.addArrangedSubview(nameLabel)
