3ed993429d5715407d6c7639204112b29429a15d
Mike <m.shevelinski@gmail.com>
2024-03-20 17:56:37 +0100

    Move sign out to menu bar

--- generics-apps/GenericsRestaurants/GenericsRestaurants/GenericsRestaurantsApp.swift
+++ generics-apps/GenericsRestaurants/GenericsRestaurants/GenericsRestaurantsApp.swift
@@ -6,12 +6,25 @@
 //
 
 import SwiftUI
+import Factory
 
 @main
 struct GenericsRestaurantsApp: App {
+
+    @Injected(\.authenticationRepository) private var repository
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .commands {
+            CommandGroup(after: .appSettings) {
+                Button("Sign out") {
+                    Task {
+                        try? await repository.signOut()
+                    }
+                }
+            }
         }
         Window("New pizza", id: "new-pizza") {
             NewMenuItemView()
