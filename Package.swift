0f7a2d74cff9961c51a30e2588e7e36f61e89551
Mike <m.shevelinski@gmail.com>
2024-07-08 20:07:50 +0200

    Add mac os app

--- Package.swift
+++ Package.swift
@@ -1,9 +1,10 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "my-project",
+    platforms: [.macOS("14.0"), .iOS("16.0")],
     dependencies: [
         .package(url: "https://github.com/Matejkob/swift-spyable.git", from: "0.2.0"),
         .package(url: "https://github.com/hmlongco/Factory.git", from: "2.3.2"),
@@ -12,5 +13,6 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"),
         .package(url: "https://github.com/soto-project/soto.git", from: "6.0.0"),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.11.2")
     ]
 )
