a51d4082dcdd2b13d1c759466ce691b9e95f4584
Mike <m.shevelinski@gmail.com>
2024-07-10 17:30:19 +0200

    Clean up and add some unit tests

---
+++ clients/generics-restaurants/tests/AppFeatureTests.swift
@@ -0,0 +1,14 @@
+//
+//  AppFeatureTests.swift
+//  GenericsRestaurantsTests
+//
+//  Created by Mike S. on 10/07/2024.
+//
+
+import XCTest
+@testable import clients_generics_restaurants_generics_restaurants
+import ComposableArchitecture
+
+class AppFeatureTests: XCTestCase {
+    
+}
