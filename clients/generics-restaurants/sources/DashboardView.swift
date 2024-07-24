a1a907957f8cc16fa82c6fa63d26137a6cde42db
Mike <m.shevelinski@gmail.com>
2024-07-24 20:29:11 +0200

    Fix some runtime warnings

--- clients/generics-restaurants/sources/DashboardView.swift
+++ clients/generics-restaurants/sources/DashboardView.swift
@@ -22,32 +22,34 @@ struct DashboardView: View {
     let store: StoreOf<DashboardFeature>
 
     var body: some View {
-        NavigationSplitView {
-            List(selection: $selected) {
-                Section("Orders") {
-                    NavigationLink("Current", value: Items.now)
-                    NavigationLink("History", value: Items.orderHistory)
+        WithPerceptionTracking {
+            NavigationSplitView {
+                List(selection: $selected) {
+                    Section("Orders") {
+                        NavigationLink("Current", value: Items.now)
+                        NavigationLink("History", value: Items.orderHistory)
+                    }
+                    Section("Other") {
+                        NavigationLink("Menu", value: Items.menu)
+                        NavigationLink("Users", value: Items.users)
+                    }
                 }
-                Section("Other") {
-                    NavigationLink("Menu", value: Items.menu)
-                    NavigationLink("Users", value: Items.users)
-                }
-            }
-        } detail: {
-            switch selected {
-            case .now:
-                NowView(store: store.scope(state: \.now,
-                                           action: \.now))
-            case .orderHistory:
-                OrderHistoryView(store: store.scope(state: \.orderHistory,
-                                                    action: \.orderHistory))
-            case .menu:
-                MenuView(store: store.scope(state: \.menu,
-                                            action: \.menu))
-            case .users:
-                if let store = store.scope(state: \.users,
-                                           action: \.users) {
-                    UsersView(store: store)
+            } detail: {
+                switch selected {
+                case .now:
+                    NowView(store: store.scope(state: \.now,
+                                               action: \.now))
+                case .orderHistory:
+                    OrderHistoryView(store: store.scope(state: \.orderHistory,
+                                                        action: \.orderHistory))
+                case .menu:
+                    MenuView(store: store.scope(state: \.menu,
+                                                action: \.menu))
+                case .users:
+                    if let store = store.scope(state: \.users,
+                                               action: \.users) {
+                        UsersView(store: store)
+                    }
                 }
             }
         }
