4c1759847c607cc6465079f18fb558e5414220a1
Mike <m.shevelinski@gmail.com>
2024-09-23 19:55:50 +0200

    Merge both Package.swift

--- Package.swift
+++ Package.swift
@@ -14,5 +14,28 @@ let package = Package(
         .package(url: "https://github.com/kylef/PathKit", from: "1.0.1"),
         .package(url: "https://github.com/soto-project/soto.git", from: "6.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.15.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "Server",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "Vapor", package: "vapor"),
+                .target(name: "SharedModels"),
+                .product(name: "PathKit", package: "PathKit"),
+                .product(name: "SotoS3", package: "soto")
+            ],
+            path: "generics-server/sources/server",
+            swiftSettings: [
+                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
+            ],
+            plugins: []
+        ),
+        .target(
+            name: "SharedModels",
+            dependencies: [],
+            path: "generics-server/sources/shared-models"
+        )
     ]
 )
