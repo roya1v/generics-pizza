d9487c6db142a377ec1a375ed7ba927f4a9fa280
Mike <m.shevelinski@gmail.com>
2024-09-26 21:20:54 +0200

    More tests

--- clients/generics-restaurants/tests/MenuFeatureTests.swift
+++ clients/generics-restaurants/tests/MenuFeatureTests.swift
@@ -3,26 +3,25 @@ import XCTest
 import ComposableArchitecture
 import Factory
 @testable import clients_libraries_GenericsCore
+import SharedModels
 
 class MenuFeatureTests: XCTestCase {
 
     var menuRepositorySpy: MenuRepositorySpy!
     var store: TestStoreOf<MenuFeature>!
 
-    @MainActor
     override func setUp() async throws {
         menuRepositorySpy = MenuRepositorySpy()
         Container.shared.menuRepository.register {
             self.menuRepositorySpy
         }
 
-        store = TestStore(initialState: MenuFeature.State()) {
+        store = await TestStore(initialState: MenuFeature.State()) {
             MenuFeature()
         }
     }
 
-    @MainActor
-    func testLoading() async {
+    func testLoadingWithSuccess() async {
         menuRepositorySpy.fetchMenuShowHiddenReturnValue = []
 
         await store.send(.shown) {
@@ -31,6 +30,46 @@ class MenuFeatureTests: XCTestCase {
 
         await store.receive(\.loaded) {
             $0.menuState = .loaded([])
+        }
+    }
+
+    func testLoadingWithFailure() async {
+        menuRepositorySpy.fetchMenuShowHiddenThrowableError = MockError()
+
+        await store.send(.shown) {
+            $0.menuState = .loading
+        }
+
+        await store.receive(\.loaded) {
+            $0.menuState = .error("mock-error")
+        }
+    }
+
+    func testNewItem() async {
+        await store.send(.newItemButtonTapped) {
+            $0.itemForm = MenuItemFormFeature.State(
+                id: nil,
+                title: "",
+                description: "",
+                price: 0.0,
+                isLoading: false,
+                errorMessage: nil
+            )
+        }
+    }
+
+    @MainActor
+    func testEditItem() async {
+        menuRepositorySpy.getImageForItemIdReturnValue = NSImage()
+        store.exhaustivity = .off(showSkippedAssertions: true)
+        let mockItem = MenuItem(id: UUID(), title: "mock", description: "mock", price: 123, isHidden: false)
+
+        await store.send(.loaded(.success([mockItem]))) {
+            $0.menuState = .loaded([MenuItemFeature.State(item: mockItem)])
+        }
+
+        await store.send(.item(.element(id: mockItem.id, action: .editTapped))) {
+            $0.itemForm = MenuItemFormFeature.State(menuItem: mockItem)
         }
     }
 }
